12e4dfbc8321c114c6241e6fa325589b611f295e
Israel Keshinro <76919894+izzykeshinbest@users.noreply.github.com>
2024-01-27 20:54:47 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,364 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-front-view-of-mdx-building-1",
+      "name": "Front View of MDX Building 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.7358209073807593,
+        "pitch": -0.15686931861886677,
+        "fov": 1.34504334089125
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.037613612201893,
+          "pitch": -0.027482356618552473,
+          "rotation": 0,
+          "target": "1-front-view-of-mdx-building-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.0709864749919689,
+          "pitch": -0.315657086006226,
+          "title": "Class Building",
+          "text": ""
+        },
+        {
+          "yaw": 2.5813597035551563,
+          "pitch": -0.36489058162690213,
+          "title": "College Building",
+          "text": "Classes, Grad, Foods, etc"
+        },
+        {
+          "yaw": -2.3759650322847428,
+          "pitch": -0.33717000610895553,
+          "title": "Williams Building",
+          "text": ""
+        },
+        {
+          "yaw": -3.0042507867263026,
+          "pitch": -0.20762938892413807,
+          "title": "Grove Building",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "1-front-view-of-mdx-building-2",
+      "name": "Front View of MDX Building 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.34504334089125
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.966554780182623,
+          "pitch": -0.1963778718600846,
+          "rotation": 0,
+          "target": "2-front-view-of-mdx-building-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.1326260641836052,
+          "pitch": -0.43871784429599714,
+          "title": "College Building",
+          "text": "Classes, Grad, Foods, etc"
+        },
+        {
+          "yaw": -1.428012416026288,
+          "pitch": -0.5794451349380267,
+          "title": "Williams Building",
+          "text": ""
+        },
+        {
+          "yaw": -2.7633159981787507,
+          "pitch": -0.31978978205120967,
+          "title": "Grove Building",
+          "text": ""
+        },
+        {
+          "yaw": -1.2758990847472216,
+          "pitch": 0.24575528406295177,
+          "title": "Car Park",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "2-front-view-of-mdx-building-3",
+      "name": "Front View of MDX Building 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.014770460900212612,
+        "pitch": 0,
+        "fov": 1.34504334089125
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.94633658355578,
+          "pitch": -0.4018211043793336,
+          "rotation": 0,
+          "target": "3-front-view-of-mdx-building-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.630888855223942,
+          "pitch": -0.3444502007481489,
+          "title": "College Building",
+          "text": "Classes, Grad, Foods, etc"
+        },
+        {
+          "yaw": -1.0629161030209584,
+          "pitch": -0.563499315726828,
+          "title": "Willams Building",
+          "text": ""
+        },
+        {
+          "yaw": -2.4804140040340883,
+          "pitch": -0.5851064251564981,
+          "title": "Grove Building",
+          "text": ""
+        },
+        {
+          "yaw": 2.985980261526036,
+          "pitch": -0.39132115036055737,
+          "title": "Sheppard Library",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "3-front-view-of-mdx-building-4",
+      "name": "Front View of MDX Building 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.34504334089125
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.911620078564317,
+          "pitch": -0.2078103317692115,
+          "rotation": 0,
+          "target": "4-front-view-to-the-sheppard-library"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-front-view-to-the-sheppard-library",
+      "name": "Front View to the Sheppard Library",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 1.312077362603766,
+        "pitch": -0.06677520132084602,
+        "fov": 1.34504334089125
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0486062260293405,
+          "pitch": -0.13270150459693753,
+          "rotation": 0,
+          "target": "5-way-to-the-unihub-office-and--sheppard-library"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-way-to-the-unihub-office-and--sheppard-library",
+      "name": "Way to the UNIHUB office and  Sheppard Library",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.5359331214405234,
+        "pitch": -0.06719655904600508,
+        "fov": 1.34504334089125
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.491485236526934,
+          "pitch": -0.037105258629813065,
+          "title": "UNIHUB HELP DESK&nbsp;",
+          "text": ""
+        },
+        {
+          "yaw": -3.0635095673779293,
+          "pitch": 0.027368483204265104,
+          "title": "Downstairs to Cafe",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "6-view-to-the-unihub-helpdesk-office",
+      "name": "View to the UNIHUB helpdesk office",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.9745436459812584,
+        "pitch": 0.10809704894305483,
+        "fov": 1.34504334089125
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.2139393069172488,
+          "pitch": 0.22579240994383554,
+          "title": "Entrance to Library",
+          "text": ""
+        },
+        {
+          "yaw": 2.9334927963901363,
+          "pitch": -0.06042744654282117,
+          "title": "HELP DESK office",
+          "text": ""
+        }
+      ]
+    }
+  ],
+  "name": "MDX Tours to Sheppard Library",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
